829f87da8587b528a5bd3b8f7f3d154e2879a61a
DmytroSkorobahatko <skorobagatko@lic145.kiev.ua>
2021-02-12 09:19:16 +0200

    Ready

---
+++ Algorithm.py
@@ -0,0 +1,95 @@
+from collections import defaultdict
+
+
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices  # V = cnt of vertexes
+        self.graph = defaultdict(list)
+        self.path = []
+
+    # function to add an edge to graph
+    def addOneEdge(self, u, v):
+        self.graph[u].append(v)  # for c: for r: eddEdge
+
+    def addTwoEdges(self, u, v):
+        self.graph[u].append(v)
+        self.graph[v].append(u)
+
+    # remove edge u-v and v-u
+    def rmvEdge(self, u, v):
+        for index, value in enumerate(self.graph[u]):
+            if value == v:
+                self.graph[u].pop(index)
+        for index, value in enumerate(self.graph[v]):
+            if value == u:
+                self.graph[v].pop(index)
+
+    # DFS to count reachable vertices from v
+    def DFSCount(self, v, visited):
+        count = 1
+        visited[v] = True
+        for i in self.graph[v]:
+            if not visited[i]:  # visited[i] == False
+                count = count + self.DFSCount(i, visited)
+        return count
+
+    # The function to check if edge u-v can be next edge in Euler Path
+    def isValidNextEdge(self, u, v):
+        # The edge u-v is valid in one of the following two cases:
+
+        # If v is the only adjacent vertex of u
+        if len(self.graph[u]) == 1:
+            return True
+        else:
+            '''vertices reachable from u'''
+            visited = [False] * self.V
+            count1 = self.DFSCount(u, visited)
+            '''Remove edge (u, v) and after removing the edge, count vertices reachable from u'''
+            self.rmvEdge(u, v)
+            visited = [False] * self.V
+            count2 = self.DFSCount(u, visited)
+            '''Add the edge back to the graph'''
+            self.addTwoEdges(u, v)
+            '''If count1 is greater, then edge (u, v) is a bridge'''
+            return False if count1 > count2 else True
+
+    # Print Euler  starting from vertex u
+    def printEulerUtil(self, u):
+        # Recur for all the vertices adjacent to this vertex
+        for v in self.graph[u]:
+
+            # If edge u-v is not removed and it's a a valid next edge
+            if self.isValidNextEdge(u, v):
+                # print("%d - %d " % (u + 1, v + 1))
+                # create path
+                if self.path:  # if path not empty
+                    self.path.pop()
+                    self.path.append(u + 1)
+                    self.path.append(v + 1)
+                else:
+                    self.path.append(u + 1)
+                    self.path.append(v + 1)
+                self.rmvEdge(u, v)
+                self.printEulerUtil(v)
+
+    def printEulerTour(self):
+        # Find a vertex with odd degree
+        u = -1
+        for i in range(self.V):
+            if len(self.graph[i]) % 2 != 0:
+                u = i
+                break
+        if u == -1:
+            print("There are no Euler Path")
+        else:
+            # Print tour starting from odd vertex
+            self.printEulerUtil(u)
+
+
+g1 = Graph(4)
+g1.addTwoEdges(0, 1)
+g1.addTwoEdges(0, 2)
+g1.addTwoEdges(1, 2)
+# g1.addTwoEdges(2, 3)
+g1.printEulerTour()
+print(g1.path)
---
+++ main.py
@@ -0,0 +1,355 @@
+import pygame as pg
+import sys
+import random
+import time
+import easygui
+from Algorithm import *
+
+'''# global options ##############################'''
+
+'''# graph sizes #####'''
+WIDTH = HEIGHT = 720
+FPS = 60
+R = 20  # vertex radius
+D = 2 * R
+EDGE_WIDTH = 2  # edge width
+FILE = open("graph.txt")
+vertexCount = 0
+isDeveloping = "the button is in Dev now. It will be ready in soon."
+
+'''# graph structure #####'''
+graphMatrix = []
+vertexConnections = {}  # v1 : v2, v3 ...
+vertexLocations = {}  # v : loc
+allVertexLocations = []  # loc1, loc2 ...
+FILE_LOADED = False
+
+'''# graph colors #####'''
+BG_COLOR = pg.Color(200, 200, 200)
+VERTEX_COLOR = pg.Color(100, 100, 250)
+EDGE_COLOR = pg.Color(0, 0, 0)
+VERTEX_VISITED_COLOR = pg.Color(250, 250, 50)
+EDGE_DELETED_COLOR = BG_COLOR
+
+'''# algorithm structure #####'''
+runAlgorithm = False
+finishAlgo = False
+dfsPath = []
+vertexes = []
+edges = []
+graph = Graph(1)
+
+'''# GRAPH LOAD ##############################'''
+
+
+def graphLoad():
+    global graphMatrix, vertexConnections
+    global vertexCount, graph
+    global FILE_LOADED
+    FILE_LOADED = True
+    file = FILE
+    graphMatrix = []
+    cols, rows = list(map(int, file.readline().split()))
+    vertexCount = cols  # cols == rows
+    for i in range(0, rows):  # [0; rows-1]
+        graphMatrix.append(list(map(int, file.readline().split()[:cols])))
+
+    graph = Graph(vertexCount)
+    matchingVertex(cols, rows)
+    placingVertex()
+
+
+def matchingVertex(cols, rows):
+    global vertexConnections
+    for c in range(cols):
+        vertexConnections[c + 1] = []
+
+    for c in range(cols):
+        for r in range(rows):
+            if graphMatrix[c][r] != 0:
+                vertexConnections[c + 1].append(r + 1)
+                graph.addOneEdge(c, r)
+
+
+def regenGraph():
+    for c in range(vertexCount):
+        for r in range(vertexCount):
+            if graphMatrix[c][r] != 0:
+                graph.addOneEdge(c, r)
+
+
+def placingVertex():
+    global vertexLocations, allVertexLocations
+    for c in range(vertexCount):
+        vertexLocations[c + 1] = random.randint(0 + D, WIDTH - D), random.randint(0 + D, HEIGHT - D)
+    allVertexLocations = []
+    for key, value in vertexLocations.items():
+        allVertexLocations.append(vertexLocations[key])
+
+
+'''# find start ##############################'''
+
+
+def findStart():
+    oddCnt = 0
+    startVertex = finishVertex = -1
+    for key, value in vertexLocations.items():
+        if len(vertexConnections[key]) % 2 == 1:
+            oddCnt += 1
+        if len(vertexConnections[key]) % 2 == 1 and oddCnt == 1:
+            startVertex = key
+        if len(vertexConnections[key]) % 2 == 1 and oddCnt == 2:
+            finishVertex = key
+    if oddCnt > 2:
+        print("There are no Euler Path")
+
+    return startVertex, finishVertex
+
+
+'''# GRAPH BUILD ##############################'''
+
+
+def graphBuilder():
+    global runAlgorithm, vertexes, edges, dfsPath, finishAlgo
+    pg.init()
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    screen.fill(BG_COLOR)
+    pg.display.set_caption("graph builder")
+    pg.display.update()
+    clock = pg.time.Clock()
+
+    txt_font = pg.font.SysFont("menu_font", 32)
+    txt_colors = [(10, 10, 10), (50, 250, 250), (250, 160, 0)]
+
+    relocation()
+    numStep = 0
+    numVert = 0
+    numEdge = 0
+    selectedVertex = None
+    runAlgorithm = False
+    finishAlgo = False
+
+    run = True
+    while run:
+        for e in pg.event.get():
+            if e.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+
+            if e.type == pg.KEYDOWN:
+                if e.key == pg.K_ESCAPE:
+                    run = False
+                if e.key == pg.K_r:
+                    screen.fill(BG_COLOR)
+                    numStep = 0
+                    numVert = 0
+                    numEdge = 0
+                    selectedVertex = None
+                    runAlgorithm = False
+                    finishAlgo = False
+                    relocation()
+
+            elif e.type == pg.MOUSEBUTTONDOWN:
+                mx, my = pg.mouse.get_pos()
+                loc = [mx, my]
+                if e.button == 1:
+
+                    for v in allVertexLocations:
+                        if v[0] - D <= loc[0] <= v[0] + D and v[1] - D <= loc[1] <= v[1] + D:
+                            if finishAlgo:
+                                numStep = 0
+                                numVert = 0
+                                numEdge = 0
+                                selectedVertex = None
+                                runAlgorithm = False
+                                finishAlgo = False
+                                graph.path = []
+                                dfsPath = []
+                                vertexes = []
+                                edges = []
+
+                            loc = v
+                            sv, fv = findStart()
+
+                            if loc == vertexLocations[sv]:  # or
+                                graph.printEulerTour()
+                                dfsPath = graph.path
+                                regenGraph()
+                                runAlgorithm = True
+                            elif loc == vertexLocations[fv]:
+                                print("It will be finish vertex")
+                            else:
+                                print("can't be start vertex")
+
+        if vertexes == dfsPath and runAlgorithm:
+            time.sleep(0.5)
+            drawText("path is " + str(dfsPath), txt_font, txt_colors[1], screen, 100, 100)
+            numStep = 0
+            runAlgorithm = False
+            finishAlgo = True
+
+        if runAlgorithm and numStep <= vertexCount * 2:
+            if numStep % 2 == 0:
+                vertexes.append(dfsPath[numVert])
+                selectedVertex = dfsPath[numVert]
+                numVert += 1
+            else:
+                numEdge += 1
+                edges.append((dfsPath[numEdge - 1], dfsPath[numEdge]))
+            numStep += 1
+            time.sleep(0.5)
+
+        drawGraph(screen, selectedVertex)  # vertexConnections, vertexLocations
+        clock.tick(FPS)
+        pg.display.flip()
+
+
+def relocation():
+    global runAlgorithm, dfsPath, vertexes, edges
+    placingVertex()
+    runAlgorithm = False
+    graph.path = []
+    dfsPath = []
+    vertexes = []
+    edges = []
+
+
+'''# GRAPH DRAW ##############################'''
+
+
+def drawGraph(screen, selectedVertex):  # vertexConnections, vertexLocations
+    txt_font = pg.font.SysFont("menu_font", 32)
+    txt_color = VERTEX_COLOR
+    screen.fill(BG_COLOR)
+    drawAllEdges(screen)  # vertexConnections
+    drawAllVertexes(screen, selectedVertex)  # vertexConnections
+    if finishAlgo:
+        drawText("path is " + str(dfsPath)[1:-1:1], txt_font, txt_color, screen, 10, 10)
+    drawText("click 'R' to rebuild graph", txt_font, txt_color, screen, 10, HEIGHT - 32)
+
+
+def drawAllVertexes(screen, selectedVertex):
+    num_font = pg.font.SysFont("numeration", R)
+    for key, value in vertexConnections.items():
+        vertexNumber = str(key)
+        vertexValue = str((len(vertexConnections[key])))
+        drawVertex(screen, VERTEX_COLOR, vertexLocations[key])
+        '''drawVertex(screen, color, loc)'''
+        drawText(vertexNumber, num_font, "Black", screen, vertexLocations[key][0] - 4, vertexLocations[key][1] - 4)
+        drawText(vertexValue, num_font, "Red", screen, vertexLocations[key][0] + R, vertexLocations[key][1] - R)
+
+    if runAlgorithm:
+        for v in vertexes:
+            vertexNumber = str(v)
+            vertexValue = str((len(vertexConnections[v])))
+            drawVertex(screen, VERTEX_VISITED_COLOR, vertexLocations[v])
+            if vertexes != dfsPath:
+                drawVertex(screen, "Cyan", vertexLocations[selectedVertex])
+            drawText(vertexNumber, num_font, "Black", screen, vertexLocations[v][0] - 4, vertexLocations[v][1] - 4)
+            drawText(vertexValue, num_font, "Red", screen, vertexLocations[v][0] + R, vertexLocations[v][1] - R)
+
+
+def drawVertex(screen, color, loc):
+    pg.draw.circle(screen, color, loc, R)
+
+
+def drawAllEdges(screen):
+    for key, value in vertexConnections.items():
+        for i in range(len(vertexConnections[key])):
+            drawEdge(screen, EDGE_COLOR, vertexLocations[key], vertexLocations[vertexConnections[key][i]],
+                     EDGE_WIDTH)
+            '''drawEdge(screen, color, startLoc, finishLoc, width)'''
+    if runAlgorithm:
+        for i in range(len(edges)):
+            drawEdge(screen, EDGE_DELETED_COLOR, vertexLocations[edges[i][0]], vertexLocations[edges[i][1]], EDGE_WIDTH)
+
+
+def drawEdge(screen, color, startLoc, finishLoc, width):
+    pg.draw.line(screen, color, startLoc, finishLoc, int(width))
+
+
+def drawText(text, font, color, surface, x, y):
+    text_obj = font.render(text, 1, color)
+    text_rect = text_obj.get_rect()
+    text_rect.topleft = (x, y)
+    surface.blit(text_obj, text_rect)
+
+
+'''# MENU ##############################'''
+
+
+def mainMenu():
+    global graphMatrix, vertexConnections, vertexCount, graph
+    global FILE
+    pg.init()
+    pg.display.set_caption('Euler Path')
+    screen = pg.display.set_mode((WIDTH, HEIGHT), 0, 32)
+    font_graph = pg.font.SysFont("menu_font", 32)
+    bg_image = pg.transform.scale(pg.image.load("bg_image_dark.jpg"), (WIDTH, HEIGHT))
+
+    txt_colors = [(10, 10, 10), (50, 250, 250), (250, 160, 0), (20, 100, 100)]
+    '''txt_colors = black, cyan, orange, darkCyan'''
+
+    bWidth = WIDTH // 3
+    bHeight = HEIGHT // 20
+    bx = WIDTH // 2 - bWidth
+    by = HEIGHT // 3 - bHeight
+    shadow = 3
+
+    click = False
+
+    while True:
+        screen.fill("black")
+        screen.blit(bg_image, pg.Rect(0, 0, WIDTH, HEIGHT))
+
+        mx, my = pg.mouse.get_pos()
+
+        button_build = pg.Rect(bx, by, bWidth, bHeight)
+        button_options = pg.Rect(bx, by + bHeight * 2, bWidth, bHeight)
+
+        if button_build.collidepoint((mx, my)):
+            if click:
+                if FILE_LOADED:
+                    graphBuilder()
+                else:
+                    graphLoad()
+                    graphBuilder()
+                    print("choose file")
+        if button_options.collidepoint((mx, my)):
+            if click:
+                filePath = easygui.fileopenbox(filetypes=["*.txt"])
+                FILE = open(filePath) if filePath else open("graph.txt")
+                graphMatrix = []
+                vertexConnections = {}
+                vertexCount = 0
+                graph = Graph(vertexCount)
+                graphLoad()
+                # update()
+
+        drawText('Build Graph', font_graph, txt_colors[0], screen, bx + shadow, by + shadow)
+        drawText('Build Graph', font_graph, txt_colors[1], screen, bx, by)
+        drawText('Choose File', font_graph, txt_colors[0], screen, bx + shadow, by + bHeight * 2 + shadow)
+        drawText('Choose File', font_graph, txt_colors[1], screen, bx, by + bHeight * 2)
+
+        clock = pg.time.Clock()
+        click = False
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    pg.quit()
+                    sys.exit()
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+
+        pg.display.update()
+        clock.tick(FPS)
+
+
+'''# RUN ##############################'''
+
+if __name__ == '__main__':
+    mainMenu()
